9d405a94c5cf34db13dd9ef52ab76d5620c0e152
shuaibu <shuaibuabdulkadir222@gmail.com>
2023-10-27 17:54:06 +0100

    some changes with regrds to cookie

--- auth/auth.go
+++ auth/auth.go
@@ -90,10 +90,6 @@ func Login(w http.ResponseWriter, r *http.Request) {
 			Name: tokenString,
 		}
 
-		c, _ := r.Cookie("token")
-		log.Println(c)
-		fmt.Printf("r.Cookies(): %v\n", r.Cookies())
-
 		tkn, _ := json.Marshal(cok)
 		w.Write(tkn)
 	}
